6dd1c67e81579bebf51f0d36e612812fbad9b748
256287 (Yashwanth K) <69190665+yashwanthabhilash@users.noreply.github.com>
2021-04-30 20:30:42 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,4 @@
-/**
- * @file main.c
- * @author MALAVIKA M(256323)
- * @brief 
- * @version 0.1
- * @date 2021-04-30
- * 
- * @copyright Copyright (c) 2021
- * 
- */
+
 #include <avr/io.h>
 #include<util/delay.h>
 #include "activity1.h"
