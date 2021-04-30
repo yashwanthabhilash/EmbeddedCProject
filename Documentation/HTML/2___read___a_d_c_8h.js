f37e55e618e1863356318e9f674fc175c41da4b1
256287 (Yashwanth K) <69190665+yashwanthabhilash@users.noreply.github.com>
2021-05-01 00:03:53 +0530

    Add files via upload

---
+++ Documentation/HTML/2___read___a_d_c_8h.js
@@ -0,0 +1,5 @@
+var 2___read___a_d_c_8h =
+[
+    [ "InitADC", "2___read___a_d_c_8h.html#aebf00c4b26377bca91c8702ced5e7927", null ],
+    [ "ReadADC", "2___read___a_d_c_8h.html#aa4b8e2a1d871469a984604dd995a2402", null ]
+];
---
+++ Documentation/HTML/4___u_a_r_t_8h.js
@@ -0,0 +1,5 @@
+var 4___u_a_r_t_8h =
+[
+    [ "display", "4___u_a_r_t_8h.html#adef7e6935a555c01944add484a3dc88a", null ],
+    [ "USART_init", "4___u_a_r_t_8h.html#a4ed3883de1458404fa0b79c524c9d5ef", null ]
+];
