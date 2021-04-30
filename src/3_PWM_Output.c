a949a5577acb2bb51c0b198879bfa705bedef1ff
256287 (Yashwanth K) <69190665+yashwanthabhilash@users.noreply.github.com>
2021-04-30 20:29:09 +0530

    Update 3_PWM_Output.c

--- src/3_PWM_Output.c
+++ src/3_PWM_Output.c
@@ -1,1 +1,29 @@
+#include<avr/io.h>
+
+/** \brief compare() will control the PWM duty cycle as per temperature ADC value
+ *
+ * \param temp variable contains the ADC value read from temp sensor/potentiometer
+ * \return none
+ *
+ */
+
+void compare(uint16_t temp)
+{
+    if(temp<=200)
+       {
+           OCR0A=50;             // PWM Duty cycle: 20%
+       }
+      else if(temp>200 && temp<=500)
+       {
+        OCR0A=101;              //PWM Duty cycle: 40%
+       }
+      else if(temp>500 && temp<=710)
+        {
+            OCR0A=177;           //PWM Duty cycle: 70%
+        }
+      else
+         {
+             OCR0A=241;         //PWM Duty cycle: 95%
+         }
+}
 
