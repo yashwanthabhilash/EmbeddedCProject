7f9cde75363bb838e5e943e90e1b5286517b53f3
256287 (Yashwanth K) <69190665+yashwanthabhilash@users.noreply.github.com>
2021-04-30 20:28:15 +0530

    Update 1_LED_Switch.c

--- src/1_LED_Switch.c
+++ src/1_LED_Switch.c
@@ -1,1 +1,18 @@
+#include <avr/io.h>
+
+    /** \brief intializes and declares all required ports
+            *
+            * \param non
+            * \return void
+            *
+            */
+void port()
+{
+    DDRB=00000001;
+    DDRD |= 0 << PIND0; //making pin 0 of port d as input pin
+    DDRD |= 0 << PIND1; //making pin 1 of port d as input pin
+    PORTD |= 1 << PIND1;//making pin 1 of port d HIGH
+    PORTD |= 1 << PIND0; //making pin 0 of port d HIGH
+    DDRD|=(1<<DDD6);
+}
 
